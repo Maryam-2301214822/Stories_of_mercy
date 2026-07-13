cf1a0d65f833882d4adbe18d42ebe83372d6ba00
Maryam-2301214822 <maryam2301214822@gmail.com>
2026-07-13 11:26:42 +0300

    Delete alsirah_store directory

--- alsirah_store/create_admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config/db.php';
-$userHash = password_hash("123456", PASSWORD_DEFAULT);
-$adminHash = password_hash("admin123", PASSWORD_DEFAULT);
-
-mysqli_query($conn, "UPDATE users SET password='$userHash' WHERE email='user@example.com'");
-mysqli_query($conn, "UPDATE users SET password='$adminHash' WHERE email='admin@example.com'");
-
-echo "تم تشفير كلمات المرور بنجاح";
